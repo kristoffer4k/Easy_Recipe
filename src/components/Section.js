b267b3017e000de121f2babb44cbb27b5e78fc88
kristoffer4k <krzysztof.korbinski@gmail.com>
2022-08-08 16:08:47 +0200

    basic structure build

---
+++ src/components/Section.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Section.scss'
+
+function Sections() {
+  return (
+    <section className='main-section'>
+
+    </section>
+  )
+}
+
+export default Sections
