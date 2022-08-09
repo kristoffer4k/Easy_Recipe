342a0b3a966d027c237abb5bfe1a57765be78e1b
kristoffer4k <krzysztof.korbinski@gmail.com>
2022-08-09 19:08:14 +0200

    icons

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import '../styles/Main.scss'
+import { FaSearch } from 'react-icons/fa';
 
 function Main() {
   return (
     <section className='main'>
       <div className='content'>
-        <h1>The easiest and fastest way to find the recipe you want!</h1>
+        <h1>The easiest and fastest way to find<br></br>the recipe you want!</h1>
+        <FaSearch className='icon'/>
       </div>
       <form className='form_1' >
-         <input type='text' placeholder='Search by dish, ingredients or keyword'/>
-         <button type='submit'>Search</button> 
+         <input type='text' className='icon' placeholder='Search by dish, ingredient or keyword'/>
+         <button  type='submit'>Search</button>
        </form> 
+       
     </section>
   )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 
+
 import App from './App';
 
 
