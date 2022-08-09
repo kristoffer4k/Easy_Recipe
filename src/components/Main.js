bb2f313307ab06e161d9e23ef1f7e4906d47f9a6
kristoffer4k <krzysztof.korbinski@gmail.com>
2022-08-09 20:30:02 +0200

    icons2

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import '../styles/Main.scss'
-import { FaSearch } from 'react-icons/fa';
+// import { FaSearch } from 'react-icons/fa';
 
 function Main() {
   return (
     <section className='main'>
       <div className='content'>
         <h1>The easiest and fastest way to find<br></br>the recipe you want!</h1>
-        <FaSearch className='icon'/>
+        
       </div>
       <form className='form_1' >
+         {/* <FaSearch className='icon'/> */}
          <input type='text' className='icon' placeholder='Search by dish, ingredient or keyword'/>
          <button  type='submit'>Search</button>
        </form> 
