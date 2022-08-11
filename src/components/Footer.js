bcf3b1602a468b9524d859b51129dfe25a805795
kristoffer4k <krzysztof.korbinski@gmail.com>
2022-08-11 19:33:54 +0200

    footer update

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,33 +1,40 @@
 import '../styles/Footer.scss'
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { BsFacebook } from 'react-icons/bs';
+import { BsInstagram } from 'react-icons/bs';
+import { BsTwitter } from 'react-icons/bs';
+import { BsPinterest } from 'react-icons/bs';
+import { BsYoutube } from 'react-icons/bs';
+
+
 
 function Footer() {
   return (
     <section className='footer'>
       <div className='newsletter'>
         <h3>Subscribe to our weekly newsletter<br></br>so that you never miss the newest recipes!</h3>
-        <input type='text' placeholder='Email address'/>
-        <button  type='submit'>Sign Up</button>
+          <div className='form_2'>
+            <input type='text' placeholder='Email address'/>
+            <button  type='submit'>Sign Up</button>
+          </div>
       </div>
       
+      
     <div className='social-media'>
-      <h4>Follow us on</h4>
       <li className='li-items'>
-          <a href='https://www.facebook.com/'>Facebook</a>
-          <a href='https://www.instagram.com/'>Insta</a>
-          <a href='https://www.twitter.com/'>Twitter</a>
-          <a href='https://www.pinterest.com/'>Pinterest</a>
-          <a href='https://www.youtube .com/'>Youtube</a>
+          <a href='https://www.facebook.com/'><BsFacebook className='icon'/></a>
+          <a href='https://www.instagram.com/'><BsInstagram className='icon'/></a>
+          <a href='https://www.twitter.com/'><BsTwitter className='icon'/></a>
+          <a href='https://www.pinterest.com/'><BsPinterest className='icon'/></a>
+          <a href='https://www.youtube.com/'><BsYoutube className='icon'/></a>
       </li>
     </div>
 
     <div className='terms-privacy'>
         <li className='li-items'>
-          <Link to='terms'>Terms and conidtions</Link>
-          <p>|</p>
-          <Link to='policy'>Privacy policy</Link>
-          <p>|</p>
+          <Link to='terms'>Terms and Conidtions</Link>
+          <Link to='policy'>Privacy Policy</Link>
           <Link to='accesibility'>Accessibility</Link>
         </li>
     </div>
