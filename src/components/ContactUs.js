676252083004603af52b7d9545c3d2f139598872
kristoffer4k <krzysztof.korbinski@gmail.com>
2022-08-15 16:28:56 +0200

    form building

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -1,18 +1,30 @@
 import '../styles/ContactUs.scss'
 import React from 'react'
+import { useForm } from "react-hook-form";
 
-function Contact() {
+const Contact = () => {
+
+  // eslint-disable-next-line
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  const onSubmit = data => alert(JSON.stringify(data));
+
   return (
       <React.Fragment>
         
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className='header-section'>
               <p>Contact Us</p>
           </div>
           
           <div>
-            <input type="text" className="input" placeholder='Full name' />
-            <div>Error</div> 
+            <input type="text" className="input" placeholder='Full name' 
+            {...register("fullName", { required: {
+              value: true,
+              message: 'Full name is required',
+              } })}/>
+            <div>
+            {errors.fullName && <span className='error'>{errors.fullName.message}</span>}  
+            </div> 
           </div>
           
           <div>
@@ -25,10 +37,10 @@ function Contact() {
           </div>
 
           <div>
-            <button className='button' type='submit' value='Submit'></button>
+            <button className='button' type='submit' value='Submit'>Submit</button>
           </div>
         </form>
-        
+
       </React.Fragment>
   )
 }
