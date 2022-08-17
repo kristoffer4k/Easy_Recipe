6fde6d4a958a166f03c1cdba67b8a07493a68dc5
kristoffer4k <krzysztof.korbinski@gmail.com>
2022-08-17 16:57:22 +0200

    under construction

--- src/components/KitchenTips.js
+++ src/components/KitchenTips.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import '../styles/KitchenTips.scss'
+import Footer from './Footer';
+import Header from './Header'
+import '../styles/KitchenTips.scss'
 
 function KitchenTips() {
   return (
     <section className='tips'>
-        <p>Kitchen Tips - Under Construction</p>
+      <Header />
+        <div className='underContruction'></div>
+        <Footer />
     </section>
   )
 }
